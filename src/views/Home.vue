135343daeafcfab98f8d2344448ea6238a389db8
Horvee <horveejin@outlook.com>
2021-11-24 21:51:58 +0800

    Change somthing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,19 @@
         <div style="margin:10px;">
             <search-bar @click-search="actionSearch" />
         </div>
+        <h3>{{ resultTotal }}</h3>
+        <el-row>
+            <el-col :span="24" v-for="item in logs" :key="item.id">
+                <el-col :span="6">
+                    <h2>{{ item.storyTitle }}</h2>
+                    <h3>{{ item.storyCode }}</h3>
+                </el-col>
+                <el-col :span="4">
+                    <h4>{{ new Date(item.startTime) }}</h4>
+                    <h4>{{ item.useTime / 1000 }} sec</h4>
+                </el-col>
+            </el-col>
+        </el-row>
         <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
         <HelloWorld msg="Welcome to Your Vue.js App" />
     </div>
@@ -21,8 +34,14 @@ export default {
         HelloWorld,
         SearchBar,
     },
+    data() {
+        return {
+            resultTotal: null,
+            logs: []
+        }
+    },
     methods: {
-        actionSearch: (data) => {
+        actionSearch(data) {
             console.log(data);
             // build es query
             let queryBody = QueryBuilder.build(data);
@@ -30,10 +49,19 @@ export default {
             Axios.post('/api/storylog/_search',queryBody)
             .then((resp) => {
                 console.log(resp);
-                if(resp.status == 200) {
-                    console.log("request is ok!")
+                if(resp.status != 200) {
+                    console.log("request fail!")
                     return;
                 }
+                
+                let data = resp.data;
+                this.$data.resultTotal = data.hits.total.value;
+
+                let logList = [];
+                for(let item of data.hits.hits) {
+                    logList.push(item._source);
+                }
+                this.$data.logs = logList;
                 
             }).catch((rejObj) => {
                 console.log('reject',rejObj)
